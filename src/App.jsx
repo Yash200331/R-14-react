de3e7f2851c1498d40168b419c4fad3da6838654
Yash20031 <ychaturvedi992@gmail.com>
2024-09-25 12:47:12 +0530

    functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,65 @@
+import React from 'react'
+
+
+
+
 const App = () => {
+  let info = [
+    {
+      name: "React",
+      version: 17,
+      status: "active",
+    },
+    {
+      name: "Angular",
+      version: 18,
+      status: "active",
+    },
+    {
+      name: "Vue",
+      version: 12,
+      status: "active",
+    },
+    {
+      name: "Stylus",
+      version: 13,
+      status: "active",
+    },
+  ]
+
+  let x = info.map((item, index) => {
+    return (
+      <div key={index}>
+        <h1>Name: {item.name}</h1>
+        <p>Version: {item.version}</p>
+        <p>Status: {item.status}</p>
+      </div>
+    )
+  })
+
+  let getinfo = () => {
+    console.log("hello world");
+  }
+
+  let takeInfo = (hello) => {
+    console.log(hello);
+  }
+
+
   return (
     <div>
-      <h1>this is react boiler plate</h1>
-      <h2>{import.meta.env.VITE_ENV_VARIABLE}</h2>
+      
+      <button onClick={getinfo}>
+        click me 
+      </button>
+
+      <button onClick={() => {takeInfo("This is info")}}>
+        print me
+      </button>
+
     </div>
+
   )
 }
 
-export default App;
+export default App
